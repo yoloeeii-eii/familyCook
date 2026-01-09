95641c9db587f5f51e0b6eba74dc2b6f4da96298
雷丽 <15573139+yolo-eii@user.noreply.gitee.com>
2026-01-09 16:41:44 +0800

    fix:taiwindcss

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import Antd from 'ant-design-vue';
-import 'ant-design-vue/dist/reset.css';  // Antd 重置样式（建议保留）
-import * as Icons from '@ant-design/icons-vue';
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/reset.css' // Antd 重置样式（建议保留）
+import * as Icons from '@ant-design/icons-vue'
 
 import App from './App.vue'
 import router from './router'
@@ -12,9 +12,9 @@ import router from './router'
 const app = createApp(App)
 
 // 1. 注册插件（顺序无关，但建议按依赖顺序）
-app.use(createPinia())       // 状态管理
-app.use(router)              // 路由
-app.use(Antd)                // UI 组件库
+app.use(createPinia()) // 状态管理
+app.use(router) // 路由
+app.use(Antd) // UI 组件库
 // 全局注册所有图标（体积较大，但使用方便）
 for (const [key, component] of Object.entries(Icons)) {
   app.component(key, component)
--- vite.config.ts
+++ vite.config.ts
@@ -6,18 +6,12 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-    tailwindcss()
-  ],
+  plugins: [vue(), vueJsx(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
